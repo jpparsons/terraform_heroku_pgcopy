e6b653fca37d7b4b78a5fe44a4903b879c828306
john <john@productplan.com>
2022-08-26 12:49:05 -0700

    use local backend

--- main.tf
+++ main.tf
@@ -2,14 +2,4 @@
 # https://github.com/heroku-examples/terraform-heroku-pipeline-slugs/blob/master/variables.tf
 # terraform apply -var-file=restores-prod.tfvars -var personal=false
 
-terraform {
-  backend "pg" {
-  }
-}
-
-provider "heroku" {
-}
-
-# TODO: heroku-formation to add webs...etc
-# https://www.terraform.io/docs/providers/heroku/r/formation.html
-# https://github.com/heroku-examples/terraform-heroku-common-kong-microservices/blob/master/main.tf
+provider "heroku" {}
